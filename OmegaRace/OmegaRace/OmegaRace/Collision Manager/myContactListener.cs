1f26735cf95be83d8f78ebfbc3573164d8f98ea8
Yuancheng Zhang <old.far.hot@gmail.com>
2016-05-06 16:07:28 -0500

    init

---
+++ OmegaRace/OmegaRace/OmegaRace/Collision Manager/myContactListener.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Box2D.XNA;
+using CollisionManager;
+using Microsoft.Xna.Framework;
+
+namespace OmegaRace
+{
+    class myContactListener : IContactListener
+    {
+
+
+
+        public  void BeginContact(Contact contact)
+        {
+            GameObject A = (GameObject)contact.GetFixtureA().GetUserData();
+            GameObject B = (GameObject)contact.GetFixtureB().GetUserData();
+
+            Manifold manifold;
+            Transform xfA;
+            Transform xfB;
+            float radiusA;
+            float radiusB;
+
+            contact.GetFixtureA().GetBody().GetTransform(out xfA);
+            contact.GetFixtureB().GetBody().GetTransform(out xfB);
+            radiusA = contact.GetFixtureA().GetShape()._radius;
+            radiusB = contact.GetFixtureB().GetShape()._radius;
+
+            contact.GetManifold(out manifold);
+
+            WorldManifold worldManifold = new WorldManifold(ref manifold,ref xfA,radiusA,ref xfB,radiusB);
+
+            Vector2 ptA = worldManifold._points[0];
+            Vector2 ptB = worldManifold._points[1];
+
+            //System.Console.Write(" point {0} {1}\n", ptA, ptB);
+
+            if (A.CollideAvailable == true && B.CollideAvailable == true)
+            {
+                if (A.type < B.type)
+                {
+                    A.Accept(B, ptA);
+                }
+                else
+                {
+                    B.Accept(A, ptA);
+                }
+            }
+
+            if (A.type == GameObjType.p1missiles || A.type == GameObjType.p2missiles)
+            {
+                A.CollideAvailable = false;
+            }
+
+            if (B.type == GameObjType.p1missiles || B.type == GameObjType.p2missiles)
+            {
+                B.CollideAvailable = false;
+            }
+        }
+
+        public  void EndContact(Contact contact)
+        {
+            
+        }
+
+        public void PostSolve(Contact contact, ref ContactImpulse impulse)
+        {
+        }
+
+        public void PreSolve(Contact contact, ref Manifold oldManifold)
+        {
+        }
+
+        
+
+
+    }
+}
---
+++ OmegaRace/OmegaRace/OmegaRace/PhysicManager/PhysicsMan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Box2D.XNA;
+using CollisionManager;
+
+namespace OmegaRace
+{
+    class PhysicsMan : Manager
+    {
+        private static PhysicsMan instance;
+
+        private PhysicsMan()
+        {
+
+        }
+
+        public static PhysicsMan Instance()
+        {
+            if (instance == null)
+                instance = new PhysicsMan();
+            return instance;
+        }
+
+        public void addPhysicsObj(GameObject _gameObj,Body _body)
+        {
+            PhysicsObj obj = new PhysicsObj(_gameObj, _body);
+            _gameObj.physicsObj = obj;
+
+            this.privActiveAddToFront((ManLink)obj, ref this.active);
+        }
+
+        public void Update()
+        {
+            ManLink ptr = this.active;
+            PhysicsObj physNode = null;
+            Body body = null;
+
+
+            while (ptr != null)
+            {
+                physNode = (PhysicsObj)ptr;
+                body = physNode.body;
+
+                physNode.gameObj.pushPhysics(body.GetAngle(), body.Position);
+
+                ptr = ptr.next;
+            }
+
+        }
+
+        public void removePhysicsObj(PhysicsObj _obj)
+        {
+            this.privActiveRemoveNode((ManLink)_obj, ref this.active);
+
+        }
+
+        protected override object privGetNewObj()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
